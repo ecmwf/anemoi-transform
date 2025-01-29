9567cb004dc552290ea55865b491bf33090e036d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-01-29 10:37:40 +0000

    feat: Add `icon_refinement_level` filter

---
+++ src/anemoi/transform/filters/icon_refinement_level.py
@@ -0,0 +1,80 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import logging
+from functools import lru_cache
+
+import numpy as np
+import tqdm
+
+from ..fields import new_field_from_latitudes_longitudes
+from ..fields import new_field_from_numpy
+from ..fields import new_fieldlist_from_list
+from ..filter import Filter
+from . import filter_registry
+
+LOG = logging.getLogger(__name__)
+
+
+@lru_cache(1)
+def icon_grid(path, refinement_level_c):
+    import xarray as xr
+
+    LOG.info(f"Reading ICON grid from {path}, refinement level {refinement_level_c}")
+    ds = xr.open_dataset(path)
+    latitudes = np.rad2deg(ds.clat[ds.refinement_level_c <= refinement_level_c].values)
+    longitudes = np.rad2deg(ds.clon[ds.refinement_level_c <= refinement_level_c].values)
+
+    LOG.info(f"Latitudes {np.min(latitudes)} {np.max(latitudes)} ({len(latitudes)})")
+    LOG.info(f"Longitudes {np.min(longitudes)} {np.max(longitudes)} ({len(longitudes)})")
+
+    LOG.info("Done")
+    return latitudes, longitudes
+
+
+@filter_registry.register("icon_refinement_level")
+class IconRefinement(Filter):
+    """A filter to regrid fields using earthkit-regrid."""
+
+    def __init__(self, *, grid, refinement_level_c):
+        self.grid = grid
+        self.refinement_level_c = refinement_level_c
+
+        self.latitudes, self.longitudes = icon_grid(self.grid, self.refinement_level_c)
+        self.nearest_grid_points = None
+
+    def forward(self, fields):
+        if self.nearest_grid_points is None:
+            from anemoi.utils.grids import nearest_grid_points
+
+            # We assume all fields have the same grid
+            latitudes, longitudes = fields[0].grid_points()
+            self.nearest_grid_points = nearest_grid_points(
+                latitudes,
+                longitudes,
+                self.latitudes,
+                self.longitudes,
+            )
+
+        result = []
+        for field in tqdm.tqdm(fields, desc="Regridding"):
+
+            data = field.to_numpy(flatten=True)
+            data = data[..., self.nearest_grid_points]
+            result.append(
+                new_field_from_latitudes_longitudes(
+                    new_field_from_numpy(data, template=field), latitudes=self.latitudes, longitudes=self.longitudes
+                )
+            )
+
+        return new_fieldlist_from_list(result)
+
+    def backward(self, data):
+        raise NotImplementedError("IconRefinement is not reversible")

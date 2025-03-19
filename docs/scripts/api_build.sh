52ba69485a47df3f4660b460fe3f0c4752b9ed14
Harrison Cook <harrison.cook@ecmwf.int>
2025-03-19 13:51:41 +0000

    Fix script

--- docs/scripts/api_build.sh
+++ docs/scripts/api_build.sh
@@ -3,5 +3,5 @@ script_dir=$(dirname "${BASH_SOURCE[0]}")
 docs_dir="$script_dir/.."
 source_dir="$script_dir/../../src/"
 
-sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" $(SPHINXOPTS) -t "$docs_dir/_templates/apidoc" $(O)
+sphinx-apidoc -M -f -o "$docs_dir/_api" "$source_dir/anemoi" -t "$docs_dir/_templates/apidoc"
 sed -i -s 's/transform\./anemoi\.transform\./g' $docs_dir/_api/*.rst

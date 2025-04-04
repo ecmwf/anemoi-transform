db0b70ad8eb00945d573c69b665fe1859b0889d9
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2025-04-04 13:30:52 +0200

    feat: add grid class and factory (#71)

--- src/anemoi/transform/grids/icon.py
+++ src/anemoi/transform/grids/icon.py
@@ -13,18 +13,21 @@ from functools import lru_cache
 
 import numpy as np
 
+from . import Grid
+from . import grid_registry
+
 LOG = logging.getLogger(__name__)
 
 
 @lru_cache(1)
-def icon_grid(path: str, refinement_level_c: int) -> tuple[np.ndarray, np.ndarray]:
+def icon_grid(path: str, refinement_level_c: int | None = None) -> tuple[np.ndarray, np.ndarray]:
     """Read the ICON grid from a file.
 
     Parameters
     ----------
     path : str
         The path to the ICON grid file.
-    refinement_level_c : int
+    refinement_level_c : int, optional
         The refinement level to use.
 
     Returns
@@ -36,11 +39,26 @@ def icon_grid(path: str, refinement_level_c: int) -> tuple[np.ndarray, np.ndarra
 
     LOG.info(f"Reading ICON grid from {path}, refinement level {refinement_level_c}")
     ds = xr.open_dataset(path)
-    latitudes = np.rad2deg(ds.clat[ds.refinement_level_c <= refinement_level_c].values)
-    longitudes = np.rad2deg(ds.clon[ds.refinement_level_c <= refinement_level_c].values)
+    if "refinement_level_c" != None:
+        idx = ds.refinement_level_c <= refinement_level_c
+    else:
+        idx = slice(None)
+    latitudes = np.rad2deg(ds.clat[idx].values)
+    longitudes = np.rad2deg(ds.clon[idx].values)
 
     LOG.info(f"Latitudes {np.min(latitudes)} {np.max(latitudes)} ({len(latitudes)})")
     LOG.info(f"Longitudes {np.min(longitudes)} {np.max(longitudes)} ({len(longitudes)})")
 
     LOG.info("Done")
     return latitudes, longitudes
+
+
+@grid_registry.register("icon")
+class IconGrid(Grid):
+
+    def __init__(self, path: str, refinement_level_c: int | None = None):
+        self.path = path
+        self.refinement_level_c = refinement_level_c
+
+    def latlon(self) -> tuple[np.ndarray, np.ndarray]:
+        return icon_grid(self.path, self.refinement_level_c)

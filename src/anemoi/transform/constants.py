6e97620114482d65eace4e9522f7b53fd43b5067
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-05-13 06:46:48 +0000

    add constants

---
+++ src/anemoi/transform/constants.py
@@ -0,0 +1,16 @@
+# (C) Copyright 2025 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from earthkit.meteo.constants.constants import R as R_universal_gas_constant
+from earthkit.meteo.constants.constants import g as g_gravitational_acceleration
+
+__all__ = [
+    "g_gravitational_acceleration",
+    "R_universal_gas_constant",
+]

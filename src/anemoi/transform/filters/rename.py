b4d44296ab67d80ee1df71fb18a7a4d48f38f968
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2025-05-13 06:41:42 +0000

    update

--- src/anemoi/transform/filters/rename.py
+++ src/anemoi/transform/filters/rename.py
@@ -7,6 +7,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+import re
+
 import earthkit.data as ekd
 
 from anemoi.transform.fields import new_field_with_metadata
@@ -15,16 +17,56 @@ from anemoi.transform.filter import Filter
 from anemoi.transform.filters import filter_registry
 
 
+class FormatRename:
+    def __init__(self, what, format):
+        self.what = what
+        self.format = format
+        self.bits = re.findall(r"{(\w+)}", format)
+
+    def rename(self, field):
+        md = field.metadata(self.what, default=None)
+        if md is None:
+            return field
+
+        values = field.metadata(*self.bits)
+        kwargs = {k: v for k, v in zip(self.bits, values)}
+        kwargs = {self.what: self.format.format(**kwargs)}
+        return new_field_with_metadata(template=field, **kwargs)
+
+
+class DictRename:
+    def __init__(self, what, renaming):
+        self.what = what
+        self.renaming = renaming
+
+    def rename(self, field):
+        md = field.metadata(self.what, default=None)
+        print(self.what, md, self.renaming)
+        if md is None:
+            return field
+
+        if md not in self.renaming:
+            return field
+
+        kwargs = {self.what: self.renaming[md]}
+        print(kwargs)
+
+        return new_field_with_metadata(template=field, **kwargs)
+
+
 @filter_registry.register("rename")
 class Rename(Filter):
 
-    def __init__(
-        self,
-        *,
-        rename: dict = None,
-    ):
+    def __init__(self, *, rename: dict):
 
-        self._rename = rename
+        self._rename = {}
+        for key, value in rename.items():
+            if isinstance(value, str):
+                self._rename[key] = FormatRename(key, value)
+            elif isinstance(value, dict):
+                self._rename[key] = DictRename(key, value)
+            else:
+                raise ValueError(f"Invalid value for rename: {value}")
 
     def forward(self, data: ekd.FieldList) -> ekd.FieldList:
 
@@ -32,10 +74,9 @@ class Rename(Filter):
 
         for field in data:
 
-            param = field.metadata("param")
-            if param in self._rename:
-                result.append(new_field_with_metadata(template=field, param=self._rename[param]))
-            else:
-                result.append(field)
+            for _, renamer in self._rename.items():
+                field = renamer.rename(field)
+
+            result.append(field)
 
         return new_fieldlist_from_list(result)

612d9838f03d283a762382dd9d6665ac29e883b2
Lorenzo Zampieri <lorenzo.zampieri@ecmwf.int>
2025-05-18 16:06:53 +0000

    Add licence header and modify min_val and max_val names

--- src/anemoi/transform/filters/clipper.py
+++ src/anemoi/transform/filters/clipper.py
@@ -1,3 +1,12 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 from typing import Iterator, Optional
 import numpy as np
 import earthkit.data as ekd
@@ -9,16 +18,16 @@ from anemoi.transform.filters.matching import matching
 
 @filter_registry.register("clipper")
 class Clipper(MatchingFieldsFilter):
-    """Clip the values of a single field to a specified range [min_value, max_value].
+    """Clip the values of a single field to a specified range [minimum_valueue, maximum_valueue].
 
     Parameters
     ----------
     param : str
         Name of the field to clip.
-    min_value : float, optional
-        Minimum allowed value. Values below this will be set to min_value.
-    max_value : float, optional
-        Maximum allowed value. Values above this will be set to max_value.
+    minimum_valueue : float, optional
+        Minimum allowed value. Values below this will be set to minimum_valueue.
+    maximum_valueue : float, optional
+        Maximum allowed value. Values above this will be set to maximum_valueue.
     """
 
     @matching(select="param", forward=("param",))
@@ -26,16 +35,16 @@ class Clipper(MatchingFieldsFilter):
         self,
         *,
         param: str,
-        min_value: Optional[float] = None,
-        max_value: Optional[float] = None,
+        minimum_valueue: Optional[float] = None,
+        maximum_valueue: Optional[float] = None,
     ):
-        if min_value is None and max_value is None:
-            raise ValueError("At least one of min_value or max_value must be specified.")
+        if minimum_valueue is None and maximum_valueue is None:
+            raise ValueError("At least one of minimum_valueue or maximum_valueue must be specified.")
         self.param = param
-        self.min_value = min_value
-        self.max_value = max_value
+        self.minimum_valueue = minimum_valueue
+        self.maximum_valueue = maximum_valueue
 
     def forward_transform(self, param: ekd.Field) -> Iterator[ekd.Field]:
         data = param.to_numpy()
-        clipped = np.clip(data, self.min_value, self.max_value)
+        clipped = np.clip(data, self.minimum_valueue, self.maximum_valueue)
         yield self.new_field_from_numpy(clipped, template=param, param=self.param)

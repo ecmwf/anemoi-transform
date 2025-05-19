c22e13ff0bae25f270ff16cc0e9f74515e19a852
anaprietonem <ana.prietonemesio@ecmwf.int>
2025-05-19 11:51:13 +0000

    fixes dewpoint

--- src/anemoi/transform/filters/dewpoint.py
+++ src/anemoi/transform/filters/dewpoint.py
@@ -8,6 +8,8 @@
 # nor does it submit to any jurisdiction.
 
 
+from typing import Iterator
+
 import earthkit.data as ekd
 from earthkit.meteo import thermo
 
@@ -16,12 +18,15 @@ from .matching import MatchingFieldsFilter
 from .matching import matching
 
 
+EPS = 1.0e-4
+
 class DewPoint(MatchingFieldsFilter):
     """A filter to extract dewpoint temperature from relative humidity and temperature"""
 
     @matching(
         select="param",
         forward=("temperature", "relative_humidity"),
+        backward=("dewpoint", "temperature"),
     )
     def __init__(self, *, relative_humidity="r", temperature="t", dewpoint="d"):
 
@@ -29,21 +34,21 @@ class DewPoint(MatchingFieldsFilter):
         self.temperature = temperature
         self.dewpoint = dewpoint
 
-    def forward_transform(self, temperature: ekd.Field, relative_humidity: ekd.Field) -> ekd.Field:
+    def forward_transform(self, temperature: ekd.Field, relative_humidity: ekd.Field) -> Iterator[ekd.Field]:
         """Return the dewpoint temperature (Td, in K) along with temperature (K) and relative humidity (in %)"""
 
-        td = thermo.dewpoint_from_relative_humidity(temperature.to_numpy(), relative_humidity.to_numpy())
-
-        yield self.new_field_from_numpy(td, template=temperature, param=self.dewpoint)
+        relative_humidity_values=relative_humidity.to_numpy()
+        relative_humidity_values[relative_humidity_values==0]=EPS
+        td = thermo.dewpoint_from_relative_humidity(t=temperature.to_numpy(), r=relative_humidity_values)
+        yield self.new_field_from_numpy(td, template=relative_humidity, param=self.dewpoint)
         yield temperature
         yield relative_humidity
 
-    def backward_transform(self, dewpoint: ekd.Field, temperature: ekd.Field) -> ekd.Field:
+    def backward_transform(self, dewpoint: ekd.Field, temperature: ekd.Field) -> Iterator[ekd.Field]:
         """This will return the relative humidity (in %) from temperature (in K) and dewpoint (Td, in K),
         along with temperature and dewpoint
         """
-        rh = thermo.relative_humidity_from_dewpoint(temperature.to_numpy(), dewpoint.to_numpy())
-
+        rh = thermo.relative_humidity_from_dewpoint(t=temperature.to_numpy(), td=dewpoint.to_numpy())
         yield self.new_field_from_numpy(rh, template=temperature, param=self.relative_humidity)
         yield dewpoint
         yield temperature

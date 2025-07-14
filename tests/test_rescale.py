406ef6d7943b9626422c4cdc5ab8b2b17463e613
Aaron Hopkinson <aaron.hopkinson@ecmwf.int>
2025-07-14 15:17:22 +0100

    test(rescale): change skip behaviour for udunits2

--- tests/test_rescale.py
+++ tests/test_rescale.py
@@ -9,10 +9,23 @@
 
 import earthkit.data as ekd
 import numpy.testing as npt
+import pytest
 from pytest import approx
 
 from anemoi.transform.filters.rescale import Convert
 from anemoi.transform.filters.rescale import Rescale
+
+
+def skip_missing_udunits2():
+    """Skip tests if udunits2 package is not available."""
+    # Can't use utils.testing.skip_missing_packages because it only fails
+    # when cfunits.Units is imported...
+    try:
+        from cfunits import Units  # noqa: F401
+
+        return lambda f: f
+    except FileNotFoundError:
+        return pytest.mark.skip(reason="udunits2 not found")
 
 
 def test_rescale(fieldlist: ekd.FieldList) -> None:
@@ -35,6 +48,7 @@ def test_rescale(fieldlist: ekd.FieldList) -> None:
     npt.assert_allclose(rescaled_back[0].to_numpy(), fieldlist[0].to_numpy())
 
 
+@skip_missing_udunits2()
 def test_convert(fieldlist: ekd.FieldList) -> None:
     """Test converting temperature from Kelvin to Celsius and back.
 
@@ -43,19 +57,16 @@ def test_convert(fieldlist: ekd.FieldList) -> None:
     fieldlist : ekd.FieldList
         The fieldlist to use for testing.
     """
-    try:
-        # rescale from K to °C
-        fieldlist = fieldlist.sel(param="2t")
-        k_to_deg = Convert(unit_in="K", unit_out="degC", param="2t")
-        rescaled = k_to_deg.forward(fieldlist)
-        assert rescaled[0].values.min() == fieldlist.values.min() - 273.15
-        assert rescaled[0].values.std() == approx(fieldlist.values.std())
-        # and back
-        rescaled_back = k_to_deg.backward(rescaled)
-        assert rescaled_back[0].values.min() == fieldlist.values.min()
-        assert rescaled_back[0].values.std() == approx(fieldlist.values.std())
-    except FileNotFoundError:
-        print("Skipping test_convert because of missing UNIDATA UDUNITS2 library, " "required by cfunits.")
+    # rescale from K to °C
+    fieldlist = fieldlist.sel(param="2t")
+    k_to_deg = Convert(unit_in="K", unit_out="degC", param="2t")
+    rescaled = k_to_deg.forward(fieldlist)
+    assert rescaled[0].values.min() == fieldlist.values.min() - 273.15
+    assert rescaled[0].values.std() == approx(fieldlist.values.std())
+    # and back
+    rescaled_back = k_to_deg.backward(rescaled)
+    assert rescaled_back[0].values.min() == fieldlist.values.min()
+    assert rescaled_back[0].values.std() == approx(fieldlist.values.std())
 
 
 if __name__ == "__main__":

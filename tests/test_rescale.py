ec9a66337f2a779c0e6f5ebe68e6789657f46c7f
Aaron Hopkinson <aaron.hopkinson@ecmwf.int>
2025-07-23 10:16:15 +0100

    test: change skip behaviour for rescale and add clear_step tests (#125)
    ## Description
    Adds a test for the `clear_step` filter. Updates the skip behaviour when
    `udunits2` is missing for the `rescale` filter to give clearer output
    when the package is missing.
    
    ## What problem does this change solve?
    Test update
    
    ## What issue or task does this change relate to?
    n/a
    
    ##  Additional notes ##
    n/a
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_rescale.py
+++ tests/test_rescale.py
@@ -9,11 +9,25 @@
 
 import earthkit.data as ekd
 import numpy.testing as npt
+import pytest
 from anemoi.utils.testing import skip_if_offline
 from pytest import approx
 
 from anemoi.transform.filters.rescale import Convert
 from anemoi.transform.filters.rescale import Rescale
+
+
+def skip_missing_udunits2():
+    """Skip tests if udunits2 package is not available."""
+    # Can't use utils.testing.skip_missing_packages because it only fails
+    # when cfunits.Units is imported...
+    # NB: cfunits depends on udunits2 which is a system library and not installed with pip
+    try:
+        from cfunits import Units  # noqa: F401
+
+        return lambda f: f
+    except FileNotFoundError:
+        return pytest.mark.skip(reason="udunits2 not found")
 
 
 @skip_if_offline
@@ -37,6 +51,7 @@ def test_rescale(fieldlist: ekd.FieldList) -> None:
     npt.assert_allclose(rescaled_back[0].to_numpy(), fieldlist[0].to_numpy())
 
 
+@skip_missing_udunits2()
 @skip_if_offline
 def test_convert(fieldlist: ekd.FieldList) -> None:
     """Test converting temperature from Kelvin to Celsius and back.
@@ -46,19 +61,16 @@ def test_convert(fieldlist: ekd.FieldList) -> None:
     fieldlist : ekd.FieldList
         The fieldlist to use for testing.
     """
-    try:
-        # rescale from K to °C
-        fieldlist = fieldlist.sel(param="2t")
-        k_to_deg = Convert(unit_in="K", unit_out="degC", param="2t")
-        rescaled = k_to_deg.forward(fieldlist)
-        assert rescaled[0].values.min() == fieldlist.values.min() - 273.15
-        assert rescaled[0].values.std() == approx(fieldlist.values.std())
-        # and back
-        rescaled_back = k_to_deg.backward(rescaled)
-        assert rescaled_back[0].values.min() == fieldlist.values.min()
-        assert rescaled_back[0].values.std() == approx(fieldlist.values.std())
-    except FileNotFoundError:
-        print("Skipping test_convert because of missing UNIDATA UDUNITS2 library, " "required by cfunits.")
+    # rescale from K to °C
+    fieldlist = fieldlist.sel(param="2t")
+    k_to_deg = Convert(unit_in="K", unit_out="degC", param="2t")
+    rescaled = k_to_deg.forward(fieldlist)
+    assert rescaled[0].values.min() == fieldlist.values.min() - 273.15
+    assert rescaled[0].values.std() == approx(fieldlist.values.std())
+    # and back
+    rescaled_back = k_to_deg.backward(rescaled)
+    assert rescaled_back[0].values.min() == fieldlist.values.min()
+    assert rescaled_back[0].values.std() == approx(fieldlist.values.std())
 
 
 if __name__ == "__main__":

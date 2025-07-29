cab23328688a943a22f5e55b16cf043621090f77
Michiel Van Ginderachter <michiel.vanginderachter@meteo.be>
2025-07-29 06:52:00 +0000

    update tests for q_to_r_height filter

--- tests/test_height_level_humidity.py
+++ tests/test_height_level_humidity.py
@@ -4,6 +4,7 @@ from anemoi.transform.filters import filter_registry
 from anemoi.transform.sources import source_registry
 from anemoi.transform.testing import convert_to_ekd_fieldlist
 import earthkit.meteo as ekm
+import earthkit.data as ekd
 
 prototype = {
     "latitudes": [10.0, 0.0, -10.0],
@@ -42,6 +43,7 @@ def test_specific_humidity_to_relative_humidity_from_file():
         "q_to_r_height",
         height=2,
         specific_humidity_at_height_level="2sh",
+        relative_humidity_at_height_level="2r",
         temperature_at_height_level="2t",
         surface_pressure="sp",
         specific_humidity_at_model_levels="q",
@@ -55,32 +57,78 @@ def test_specific_humidity_to_relative_humidity_from_file():
     # output should have 5 params (2d, 2sh, 2t, sp and new 2rh), t and q at height levels are dropped
     assert len(list(output)) == 5 
 
+    # Relative humidity calculated by the filter
     relative_humidity_transform_output = convert_to_ekd_fieldlist(output).sel(
         param="2r"
-    ).to_numpy()   
-    temperature_transform_output = convert_to_ekd_fieldlist(output).sel(
-        param="2t"
-    ).to_numpy()
-    dewpoint_transform_output = convert_to_ekd_fieldlist(output).sel(
-        param="2d"
-    ).to_numpy()
+    )[0].to_numpy()   
 
-    # Input doesn't have relative humidity, but we can get it from 
-    # the dewpoint temperature
-    relative_humidity_input = ekm.thermo.relative_humidity_from_dewpoint(
-        t = temperature_transform_output,
-        td = dewpoint_transform_output
+
+    # The reference relative humidity
+    output = source_registry.create(
+        "testing", dataset="anemoi-transform/filters/input_single_level_specific_humidity_to_relative_humidity.grib"
     )
+    relative_humidity_output = source_registry.create(
+        "testing", dataset="anemoi-transform/filters/single_level_relative_humidity.npy"
+    ).ds.to_numpy()
 
-    # TODO: Find out what the correct rtol is!
-    np.testing.assert_allclose(relative_humidity_transform_output, relative_humidity_input)
+    np.testing.assert_allclose(relative_humidity_transform_output, relative_humidity_output)
 
 def test_specific_humidity_to_relative_humidity():
     pass
 
 
 def test_relative_humidity_to_specific_humidity_from_file():
-    pass
+
+    source = source_registry.create(
+        "testing", dataset="anemoi-transform/filters/input_single_level_specific_humidity_to_relative_humidity.grib"
+    )
+    relative_humidity_at_height_level = source_registry.create(
+        "testing", dataset="anemoi-transform/filters/single_level_relative_humidity.npy"
+    ).ds.to_numpy()
+
+    md = source.ds.sel(param="2d")[0].metadata().override(edition=2, shortName="2r")
+    
+    source.ds += ekd.FieldList.from_array(
+            relative_humidity_at_height_level,
+            md
+        )
+    
+    print(source.ds.metadata("param"))
+    
+    
+    # IFS A and B coeffients for level 137 - 129
+    AB_coefficients = {
+        "A": [424.414063, 302.476563, 202.484375, 122.101563, 62.781250, 22.835938, 3.757813, 0.0, 0.0],
+        "B": [0.969513, 0.975078, 0.980072, 0.984542, 0.988500, 0.991984, 0.995003, 0.997630, 1.000000],
+    }
+
+    r_to_q_height = filter_registry.create(
+        "r_to_q_height",
+        height=2,
+        specific_humidity_at_height_level="2q",
+        relative_humidity_at_height_level="2r",
+        temperature_at_height_level="2t",
+        surface_pressure="sp",
+        specific_humidity_at_model_levels="q",
+        temperature_at_model_levels="t",
+        AB=AB_coefficients,
+    )
+
+    output = source | r_to_q_height
+
+    # Input has 7 params (2sh, 2d, 2r, 2t, sp, t and q) 
+    # output should have 6 params (2sh, 2d, 2r, 2t, sp and new 2q), t and q at height levels are dropped
+    assert len(list(output)) == 6 
+
+    specific_humidity_transform_output = convert_to_ekd_fieldlist(output).sel(
+        param="2q"
+    ).to_numpy()   
+
+    specific_humidity_input = convert_to_ekd_fieldlist(source).sel(
+        param="2sh"
+    ).to_numpy()
+
+    np.testing.assert_allclose(specific_humidity_transform_output, specific_humidity_input)
 
 
 def test_relative_humidity_to_specific_humidity():

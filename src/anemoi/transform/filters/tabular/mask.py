548e2fabf18a2ed1197b03969ce7eb179c3e8608
Aaron Hopkinson <197336788+aaron-hopkinson@users.noreply.github.com>
2026-02-25 10:25:13 +0000

    feat: import tabular filters (#216)
    ## Description
    Imports many of the tabular filters from ai-obs-experimental-data @
    ca675ea and 8d4b80b
    (Note that there are still a small number of filters yet to be ported).
    
    ## What problem does this change solve?
    New feature - adds some initial tabular filters.
    
    ## What issue or task does this change relate to?
    #193
    
    ##  Additional notes ##
    So far, only minimal changes have been made to the code in order to get
    it to work under the this codebase - but the code has already been
    structured such that these filters should look similar to the gridded
    filters. As such, there are temporarily parallel implementations of a
    base class and registry etc, which should be removed and aligned with
    the existing `anemoi-transform` implementations.
    
    Note that a change will be required to `anemoi-datasets` in order for it
    to pick up the new filters.
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/anemoi/transform/filters/tabular/mask.py
@@ -0,0 +1,53 @@
+# (C) Copyright 2026 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import logging
+
+import numpy as np
+import pandas as pd
+
+from anemoi.transform.filters.tabular import TabularFilter
+from anemoi.transform.filters.tabular.support.utils import raise_if_df_missing_cols
+
+
+class MaskValues(TabularFilter, registry_name="mask"):
+    """Mask the columns of a DataFrame based on a condition.
+
+    The configuration should be a dictionary with column names as keys and
+    the mask condition (a string) as a value. This string can contain "inf" to
+    represent infinity.
+
+    Examples
+    --------
+    .. code-block:: yaml
+
+      input:
+        pipe:
+          - source:
+              ...
+          - mask:
+              foo: "lambda x: x >= 2"
+
+    """
+
+    def __init__(self, **config):
+        if not config:
+            raise ValueError("No columns to mask were specified.")
+        self.config = config
+
+    def forward(self, obs_df: pd.DataFrame) -> pd.DataFrame:
+        raise_if_df_missing_cols(obs_df, self.config.keys())
+
+        for col, mask_condition in self.config.items():
+            logging.info(f"Masking {col} with condition: {mask_condition}")
+            namespace = {"inf": np.inf}
+            mask_condition = eval(mask_condition, namespace)
+            obs_df[col] = obs_df[col].mask(mask_condition)
+        return obs_df
---
+++ tests/tabular_filters/test_superob.py
@@ -0,0 +1,114 @@
+# (C) Copyright 2026 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+import os
+
+import pandas as pd
+import pytest
+
+from tests.utils import create_tabular_filter as create_filter
+
+NO_MARS = not os.path.exists(os.path.expanduser("~/.ecmwfapirc"))
+
+
+@pytest.mark.skipif(NO_MARS, reason="No access to MARS")
+def test_superob():
+    config = {
+        "grid": "o96",
+        "timeslot_length": 3600,
+        "columns_to_take_nearest": ["datetime"],
+        "columns_to_groupby": ["reportype"],
+    }
+    df = pd.DataFrame(
+        {
+            "datetime": [
+                pd.Timestamp("2025-01-01 00:00:00"),
+                pd.Timestamp("2025-01-01 00:00:01"),
+                pd.Timestamp("2025-01-01 02:00:01"),
+                pd.Timestamp("2025-01-01 02:00:02"),
+            ],
+            "latitude": [89.1, 89.3, 89.2, 89.2],
+            "longitude": [-126, -126, -90, -90],
+            "reportype": [1001, 1001, 1001, 1001],
+            "obsvalue_rawbt_1": [207, 209, 265, 266],
+        }
+    )
+    superob = create_filter("superob", **config)
+    result = superob(df.copy())
+    expect = pd.DataFrame(
+        {
+            "datetime": [
+                pd.Timestamp("2025-01-01 00:00:01"),
+                pd.Timestamp("2025-01-01 02:00:01"),
+            ],
+            "latitude": [89.2, 89.2],
+            "longitude": [-126.0, -90.0],
+            "spatial_index": [13.0, 15.0],
+            "reportype": [1001, 1001],
+            "obsvalue_rawbt_1": [208.0, 265.5],
+        }
+    )
+    print(result)
+    print(expect)
+    pd.testing.assert_frame_equal(
+        result[expect.columns].reset_index(drop=True),
+        expect.reset_index(drop=True),
+        check_dtype=True,
+        check_column_type=True,
+        check_names=True,
+    )
+
+
+@pytest.mark.skipif(NO_MARS, reason="No access to MARS")
+def test_superob_groupby():
+    config = {
+        "grid": "o96",
+        "timeslot_length": 3600,
+        "columns_to_take_nearest": ["datetime"],
+        "columns_to_groupby": ["reportype"],
+    }
+    df = pd.DataFrame(
+        {
+            "datetime": [
+                pd.Timestamp("2025-01-01 00:00:00"),
+                pd.Timestamp("2025-01-01 00:00:01"),
+                pd.Timestamp("2025-01-01 02:00:01"),
+                pd.Timestamp("2025-01-01 02:00:02"),
+            ],
+            "latitude": [89.1, 89.3, 89.2, 89.2],
+            "longitude": [-126.1, -126.3, -90, -90],
+            "reportype": [1001, 1001, 1001, 1002],
+            "obsvalue_rawbt_1": [207, 209, 265, 266],
+        }
+    )
+    superob = create_filter("superob", **config)
+
+    result = superob(df.copy())
+    expect = pd.DataFrame(
+        {
+            "datetime": [
+                pd.Timestamp("2025-01-01 00:00:00"),
+                pd.Timestamp("2025-01-01 02:00:01"),
+                pd.Timestamp("2025-01-01 02:00:02"),
+            ],
+            "latitude": [89.2, 89.2, 89.2],
+            "longitude": [-126.2, -90.0, -90.0],
+            "spatial_index": [13.0, 15.0, 15.0],
+            "reportype": [1001, 1001, 1002],
+            "obsvalue_rawbt_1": [208.0, 265.0, 266.0],
+        }
+    )
+    print(result)
+    print(expect)
+    pd.testing.assert_frame_equal(
+        result[expect.columns].reset_index(drop=True),
+        expect.reset_index(drop=True),
+        check_dtype=True,
+        check_column_type=True,
+        check_names=True,
+    )

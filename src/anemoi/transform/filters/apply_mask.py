47881880489ef5735ac74b0595deb7ac9ce35eac
Harrison Cook <harrison.cook@ecmwf.int>
2026-03-18 16:45:57 +0000

    feat: Copy changes to apply_mask from #250, #251, #254

--- src/anemoi/transform/filters/apply_mask.py
+++ src/anemoi/transform/filters/apply_mask.py
@@ -12,9 +12,7 @@ import logging
 import earthkit.data as ekd
 import numpy as np
 
-from anemoi.transform.fields import new_field_from_numpy
-from anemoi.transform.fields import new_fieldlist_from_list
-from anemoi.transform.filter import Filter
+from anemoi.transform.filter import SingleFieldFilter
 from anemoi.transform.filters import filter_registry
 
 LOG = logging.getLogger(__name__)
@@ -36,22 +34,69 @@ OPERATORS = {
 
 
 @filter_registry.register("apply_mask")
-class MaskVariable(Filter):
-    """A filter to mask variables using external file."""
+class MaskVariable(SingleFieldFilter):
+    """A filter to mask variables using an external file.
 
-    def __init__(
-        self,
-        *,
-        path: str,
-        mask_value: float = None,
-        threshold: float = None,
-        threshold_operator: str = ">",
-        rename: str = None,
-    ):
-        """Initialize the MaskVariable filter.
+    The values of every filtered fields are set to NaN when they are either:
 
-        Parameters
-        ----------
+    - equal to `mask_value` if provided, or
+    - meeting a `threshold` condition if provided.
+
+    The variable can then optionally be renamed by appending `_{rename}` to the original name.
+
+    Examples
+    --------
+
+    .. code-block:: yaml
+
+      input:
+        pipe:
+          - source: # Can be `mars`, `netcdf`, etc.
+              param: ...
+          - apply_mask:
+              path: /path/to/mask_file.grib # E.g. a land-sea mask
+              mask_value: 0 # Will set to NaN all values where mask == 0 (i.e. sea points)
+              rename: masked # The new variable will be named `{param}_masked`
+
+    And with a threshold:
+
+    .. code-block:: yaml
+
+        input:
+          pipe:
+            - source: # Can be `mars`, `netcdf`, etc.
+               param: ...
+            - apply_mask:
+                path: /path/to/mask_file.nc # E.g. a land-sea mask
+                threshold_operator: "<=" # Operator to use for thresholding
+                threshold: 0.5 # Will set to NaN all values where mask <= 0.5
+
+    Notes
+    -----
+
+    The mask file should contain a variable with the same grid as the input data.
+
+    """
+
+    # path: str
+    required_inputs = ("path",)
+    # mask_value: float | None,
+    # threshold: float | None,
+    # threshold_operator: Literal["<", "<=", ">", ">=", "==", "!="],
+    # rename: str | None,
+    # param: str | None,
+    optional_inputs = {
+        "mask_value": None,
+        "threshold": None,
+        "threshold_operator": ">",
+        "rename": None,
+        "param": None,
+    }
+
+    def prepare_filter(self):
+        """Setup the MaskVariable filter.
+
+        Note:
         path : str
             Path to the external file containing the mask.
         mask_value : int, optional
@@ -62,48 +107,49 @@ class MaskVariable(Filter):
             New name for the masked variable, by default None.
         """
 
-        mask = ekd.from_source("file", path)[0].to_numpy(flatten=True)
+        if self.path.endswith(".npy"):
+            mask = np.load(self.path)
+        else:
+            mask = ekd.from_source("file", self.path)[0].to_numpy(flatten=True)
 
-        if mask_value is None and threshold is None:
+        if self.mask_value is None and self.threshold is None:
             raise ValueError("Either `mask_value` or `threshold` must be provided.")
 
-        if threshold is not None:
-            if threshold_operator not in OPERATORS:
+        if self.threshold is not None:
+            if self.threshold_operator not in OPERATORS:
                 raise ValueError(
-                    f"Invalid threshold operator: {threshold_operator}. "
+                    f"Invalid threshold operator: {self.threshold_operator}. "
                     f"Valid operators are: {', '.join(OPERATORS.keys())}."
                 )
-            self._mask = OPERATORS[threshold_operator](mask, threshold)
+            self.mask = OPERATORS[self.threshold_operator](mask, self.threshold)
         else:
-            self._mask = mask == mask_value
+            self.mask = mask == self.mask_value
 
-        self._rename = rename
+    def forward_select(self):
+        if self.param is not None:
+            return {"param": self.param}
+        return {}
 
-    def forward(self, data: ekd.FieldList) -> ekd.FieldList:
-        """Apply the forward transformation to the data.
+    def forward_transform(self, field: ekd.Field) -> ekd.Field:
+        """Apply the forward transformation to the field.
 
         Parameters
         ----------
-        data : ekd.FieldList
-            Input data to be transformed.
+        field : ekd.Field
+            Input field to be transformed.
 
         Returns
         -------
-        ekd.FieldList
-            Transformed data.
+        ekd.Field
+            Transformed field.
         """
-        result = []
-        extra = {}
-        for field in data:
+        metadata = {}
+        values = field.to_numpy(flatten=True)
+        values[self.mask] = np.nan
 
-            values = field.to_numpy(flatten=True)
-            values[self._mask] = np.nan
+        if self.rename is not None:
+            param = field.metadata("param")
+            name = f"{param}_{self.rename}"
+            metadata["param"] = name
 
-            if self._rename is not None:
-                param = field.metadata("param")
-                name = f"{param}_{self._rename}"
-                extra["param"] = name
-
-            result.append(new_field_from_numpy(values, template=field, **extra))
-
-        return new_fieldlist_from_list(result)
+        return self.new_field_from_numpy(values, template=field, **metadata)

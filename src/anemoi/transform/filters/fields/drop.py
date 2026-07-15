023aa60b362104e2b62ce30e74dc8dcdc4bd9efc
Harrison Cook <Harrison.cook@ecmwf.int>
2026-07-15 15:03:26 +0100

    feat: Drop filter for fields (#328)
    ## Description
    Introduces a "drop" filter for fields.
    
    **Testing:**
    
    - Added tests for dispatching behavior and tabular/field drop cases in
    `tests/dispatching_filters/test_drop.py`.
    - Added detailed tests for field-based drop scenarios in
    `tests/field_filters/test_drop.py`.
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/Contributor-License-Agreement.md)

---
+++ src/anemoi/transform/filters/fields/drop.py
@@ -0,0 +1,46 @@
+# (C) Copyright 2026 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+import earthkit.data as ekd
+
+from anemoi.transform.filter import SingleFieldFilter
+
+
+class Drop(SingleFieldFilter):
+    """A filter to drop based on metadata selection"""
+
+    def __init__(self, param: str | list[str] | None = None, levelist: int | list[int] | None = None):
+        """Initialise the Drop filter.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            Metadata selection criteria for dropping fields.
+        """
+        self.selection_criteria = {}
+        if param is not None:
+            self.selection_criteria["param"] = param
+        if levelist is not None:
+            self.selection_criteria["levelist"] = levelist
+        super().__init__()
+
+    def forward_select(self):
+        return self.selection_criteria
+
+    def backward_select(self):
+        return self.selection_criteria
+
+    def forward_transform(self, drop_field: ekd.Field) -> None:
+        """Drop fields based on metadata selection."""
+        return None
+
+    def backward_transform(self, drop_field: ekd.Field) -> None:
+        """Drop fields based on metadata selection."""
+        return None
